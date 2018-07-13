92d61283b8db34ca1fdf59b471b38a33cf58a9cf
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-13 12:35:54 +0530

    React Router and Navigation routes added

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import Discussion from './discussion';
-import Rules from './rules';
-import Workflow from './workflow';
+import Navigation from './navigation';
+import PageContent from './page-content';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>DevCamp React Starter</h1>
-        <Discussion />
-        <Rules />
-        <Workflow />
+        <Navigation />
+        <PageContent />
       </div>
     );
   }
---
+++ src/components/page-content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Discussion from './discussion';
+import Rules from './rules';
+import Workflow from './workflow';
+
+export default function() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Discussion}/>
+        <Route exact path="/rules" component={Rules}/>
+        <Route exact path="/workflow" component={Workflow}/>
+      </Switch>
+    </div>
+  )
+}
