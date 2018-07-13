b6a4cbc82d8d7b57f421cc57a8a3c52e1d8331c6
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-13 13:37:21 +0530

    Lifecycle workflow in console one by one

--- src/components/workflow.js
+++ src/components/workflow.js
@@ -1,9 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Workflow extends Component {
+export default class Workflow extends Component {
+  // First
+  constructor() {
+    super();
+    this.state = {
+      pageTitle: "workflow"
+    };
+    console.log("constructor");
+  }
+
+  // Second
+  static getDerivedStateFromProps() {
+    console.log("getDerivedStateFromProps");
+  }
+
+  // Fourth
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  // Fith (after update)
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  // Seventh (after update)
+  getSnapshotBeforeUpdate() {
+    console.log("getSnapshotBeforeUpdate");
+    return true;
+  }
+
+  // Eighth (after update)
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  // Last
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  handleClick = () => {
+    console.log("button clicked");
+    this.setState({ pageTitle: "Workflow" });
+  };
+
+  handleKeyUp = e => {
+    this.setState({ inputDetails: e.target.value });
+  };
+
+  // Third
+  // Sixth (after update)
   render() {
-    return <h1>Workflow...</h1>;
+    console.log("render");
+
+    return (
+      <div>
+        <h1>Workflow</h1>
+
+        <input type="text" onKeyUp={e => this.handleKeyUp(e)} />
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
   }
 }
-
-export default Workflow;
