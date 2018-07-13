28cc7795107b777de52a8d9e108c62bfe3d03e4a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-07-13 13:49:35 +0530

    Object Destructuring

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Discussion extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pageTitle: "Discussion",
+      currentTime: String(new Date())
+    };
+  }
   render() {
+    const { pageTitle, currentTime } = this.state;
     return (
       <div>
-        Discussion...
+        <h1>{pageTitle}</h1>
+        <div>{currentTime}</div>
       </div>
-    )
+    );
   }
 }
